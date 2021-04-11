0ddcc8dca0877f67968a9ae810b3bddb7a7dd68e
AnastasPetrenski <hloro83@gmail.com>
2021-04-11 20:50:18 +0300

    Arrays

--- 02 Arrays/Exercise/3.Add and Remove Elements .js
+++ 02 Arrays/Exercise/3.Add and Remove Elements .js
@@ -1,21 +1,21 @@
 function commandBuilder(array){
     let numbers = [];
-    console.log(array.join(' '))
+    
     for (let i = 0; i < array.length; i++) {
         if (array[i] == 'add') {
             numbers.push(i+1);
         }else{
             numbers.pop();
         }
-        
-        if (numbers.length == 0) {
-            numbers[0] = 'Empty';
-            return numbers;
-        } else {
-            return numbers;    
-        }
-        
     }
+
+    if (numbers.length == 0) {
+        numbers[0] = 'Empty';
+        return numbers;
+    } else {
+        return numbers;    
+    }
+
 }
 
 let numbers = commandBuilder(['add','add','add','add'])
